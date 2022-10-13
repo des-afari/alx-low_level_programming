da00a3b9bc794128f8a9b112e6cdf23b462f9d28
Desmond Afari <afaridesmond@gmail.com>
2022-10-13 13:02:06 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point
  *
+ *Description: c program that used 'puts' to print out a string
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
