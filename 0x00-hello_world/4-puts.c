8fa07c4156cc9b6088939a4081bd85c1d40defb2
des-afari <afaridesmond@gmail.com>
2022-10-13 13:07:06 +0000

    task 4 up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,6 @@
  * Return: Always 0 (Success)
  */
 
-char str[] = "Programming is like building a multilingual puzzle";
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
