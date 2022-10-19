ce7a9710be7e372beb80c60b594be24926c55b9c
des-afari <afaridesmond@gmail.com>
2022-10-19 05:55:34 +0000

    task 11 u

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,25 +10,19 @@
  */
 
 
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
 		while (n > 98)
-		{
-			printf("%i", n--);
-		}
-
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
+
 	else
 	{
 		while (n < 98)
-		{
-			printf("%i", n++);
-		}
-
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
-
 }
