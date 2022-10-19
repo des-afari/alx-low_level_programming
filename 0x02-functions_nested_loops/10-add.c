b8c4160d2e83acfb8c936e134171b369aa63872b
des-afari <afaridesmond@gmail.com>
2022-10-19 05:30:19 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: The first integer to be added
+ *
+ * @b: The second integer to be added
+ *
+ * Return: 0
+ *
+ */
+
+
+int add(int a, int b)
+{
+	int value = a + b;
+
+	return (value);
+}
