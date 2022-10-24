a4eae252287c1aacdbf3c3ec11fd82f053001a41
des-afari <afaridesmond@gmail.com>
2022-10-24 20:57:20 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values for two variables
+ *
+ * @a: first variable
+ *
+ * @b: second variable
+ *
+ * Return: null
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
