b18c66185bc7987b2940901daa7a81de3c3cdbc4
des-afari <afaridesmond@gmail.com>
2022-10-24 21:09:56 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenth of a string
+ *
+ * @c: string variable
+ *
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int _strlen(char *c)
+{
+	return (int strlen(*c));
+}
+
