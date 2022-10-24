2bcbf22ba89e0dd9bddb8f8d3d86194d53d0f590
des-afari <afaridesmond@gmail.com>
2022-10-24 22:20:08 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: string variable
+ *
+ * Return: null
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
