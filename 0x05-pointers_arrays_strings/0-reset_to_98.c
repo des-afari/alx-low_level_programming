a16bb049ec04188888ceffdb76cf40833f6f07bc
des-afari <afaridesmond@gmail.com>
2022-10-24 20:41:41 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that changes value to 98
+ *
+ * @n: pointer to n
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
