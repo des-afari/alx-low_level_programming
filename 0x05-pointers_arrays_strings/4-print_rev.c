cb1fd820362ce68f67abc677f1ef7241749e2e55
des-afari <afaridesmond@gmail.com>
2022-10-24 22:10:59 +0000

    task 4 update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev - function that reverses a string
+ * print_rev - function that reverses a string
  *
  * @s: string variable
  *
@@ -9,21 +9,19 @@
  *
  */
 
-void _print_rev(char *s)
+void print_rev(char *s)
 {
 	int c = 0;
 
-	while(s[c] != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
 	}
 
-	for (c -= 1;c >= 0;c--)
+	for (c -= 1; c >= 0; c--)
 	{
 		_putchar(s[c]);
 	}
 
 	_putchar('\n');
 }
-}
-
