f99c62db879a8d862ef7532b48c78ccb11173912
des-afari <afaridesmond@gmail.com>
2022-11-16 22:07:22 +0000

    task 2 upd

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int n = 0;
 	int result;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
 	if (array != NULL && cmp != NULL)
 	{
-		if (size <= 0)
-		{
-			return (-1);
-		}
+
 		for (n = 0; n < size; n++)
 		{
 			result = cmp(array[n]);
