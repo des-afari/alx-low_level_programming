7caa1d8c84c8d70819bd95ffc24b012c03420b68
des-afari <afaridesmond@gmail.com>
2022-11-16 22:11:05 +0000

    task 222

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  * int_index - searches for an integer
@@ -13,12 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int n = 0;
 	int result;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
 	if (array != NULL && cmp != NULL)
 	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
 
 		for (n = 0; n < size; n++)
 		{
@@ -30,6 +30,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 				return (n);
 			}
 		}
+
+		return (-1);
 	}
-	return (-1);
 }
