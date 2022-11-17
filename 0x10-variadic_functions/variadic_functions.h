2eaef538580d5bd06978e684e1053f4c7d973e4a
des-afari <afaridesmond@gmail.com>
2022-11-17 14:20:41 +0000

    task 1a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #include<stdarg.h>
+#include <stdio.h>
+
 /**
  * struct print - multiple choice print
  * @x: char Type of print
