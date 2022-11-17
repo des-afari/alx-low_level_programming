dc979f7bc9d2ebd6424dca098d34bed274628f76
des-afari <afaridesmond@gmail.com>
2022-11-17 13:41:48 +0000

    task 1a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,15 +9,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, j, sum = 0;
-
 	if (n == 0)
 		return (0);
-
-	else if (n == NULL)
-		return (NULL);
-
+	
 	va_list args;
+
+	int i, j, sum = 0;
 
 	va_start(args, n);
 
